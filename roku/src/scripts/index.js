865e905d36e5734e6051db42e284587a376f821e
adityanegara <aditnegarabisnis@gmail.com>
2021-06-21 10:35:43 +0700

    Roku : Add dicoding dependencies

--- roku/src/scripts/index.js
+++ roku/src/scripts/index.js
@@ -2,5 +2,5 @@ import "../styles/main.scss";
 import {main} from './starter/main.js'
 console.log("aditya negara");
 const testingBabel = "Testing Babel";
-console.log("wele")
+console.log("wele2")
 main();
